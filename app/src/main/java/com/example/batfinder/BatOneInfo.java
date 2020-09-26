e12703fc7c9bbc9071cb5a380f09913c358cfdd4
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2020-09-26 14:53:56 -0400

    test

---
+++ app/src/main/java/com/example/batfinder/BatOneInfo.java
@@ -0,0 +1,14 @@
+package com.example.batfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BatOneInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bat_one_info);
+    }
+}
