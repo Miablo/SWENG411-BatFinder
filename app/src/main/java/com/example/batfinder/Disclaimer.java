fb2dc38618f6f8feaa60c89b0375989350fbc9da
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2020-10-16 21:56:59 -0400

    add a text reader to disclaimer page and started txt file under raw will need to type up disclaimer out full only say hi right now.

--- app/src/main/java/com/example/batfinder/Disclaimer.java
+++ app/src/main/java/com/example/batfinder/Disclaimer.java
@@ -6,13 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Disclaimer extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclamier);
+
+        textView = findViewById(R.id.textView);
+        ReadTextFile();
+
 
         Button button = findViewById(R.id.acept);
         button.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +32,28 @@ public class Disclaimer extends AppCompatActivity {
                 openTask();
             }
         });
+    }
+
+    public void ReadTextFile(){
+        String string = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        InputStream is = getResources().openRawResource(R.raw.dis);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        while (true) {
+            try {
+                if ((string = reader.readLine()) == null) break;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            stringBuilder.append(string).append("\n");
+            textView.setText(stringBuilder);
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void openTask(){
