995d2918591b3acf6152a3807db3dac345c6840b
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2020-11-07 14:30:29 -0500

    Add bat text reader to info page.

--- app/src/main/java/com/example/batfinder/InfoPaigeMain.java
+++ app/src/main/java/com/example/batfinder/InfoPaigeMain.java
@@ -12,17 +12,45 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.BreakIterator;
+
 public class InfoPaigeMain extends AppCompatActivity {
 
+    TextView textView;
+    InputStream is;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_paige_main);
+        textView = findViewById(R.id.alcathoeText);
+        is = getResources().openRawResource(R.raw.barbastelle_statistic);
+        ReadTextFile();
 
     }
 
-    private void readFile(TextView tview){
-        
+    public void ReadTextFile(){
+        String string = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        while (true) {
+            try {
+                if ((string = reader.readLine()) == null) break;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            stringBuilder.append(string).append("\n");
+            textView.setText(stringBuilder);
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
