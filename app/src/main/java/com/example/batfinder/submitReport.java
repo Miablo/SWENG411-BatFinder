481bc495fa65c4fe146146ccb68dd5c336aed570
miablo <mvd5044@psu.edu>
2020-11-18 21:03:04 -0500

    submit report updates

---
+++ app/src/main/java/com/example/batfinder/submitReport.java
@@ -0,0 +1,14 @@
+package com.example.batfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class submitReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_report);
+    }
+}
